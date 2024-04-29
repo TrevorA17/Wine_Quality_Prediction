aedb1dc5beee386c3481a4b00312b44dfc04a3c2
Trevor Augustine <122263629+TrevorA17@users.noreply.github.com>
2024-04-29 16:25:31 +0300

    load dataset for preprocessing

---
+++ PreprocessingandDataTransformation.R
@@ -0,0 +1,28 @@
+# Load the wine quality dataset
+wine_data <- read.csv("data/WineQT.csv", colClasses = c(
+  fixed_acidity = "numeric",
+  volatile_acidity = "numeric",
+  citric_acid = "numeric",
+  residual_sugar = "numeric",
+  chlorides = "numeric",
+  free_sulfur_dioxide = "numeric",
+  total_sulfur_dioxide = "numeric",
+  density = "numeric",
+  pH = "numeric",
+  sulphates = "numeric",
+  alcohol = "numeric",
+  quality = "numeric",
+  Id = "character"
+), header = TRUE)
+
+# Display the structure of the dataset
+str(wine_data)
+
+# View the first few rows of the dataset
+head(wine_data)
+
+# Remove the last column
+wine_data <- wine_data[, -ncol(wine_data)]
+
+# Open the dataset in a viewer window
+View(wine_data)
