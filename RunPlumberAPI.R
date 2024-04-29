e09453a4a53b525f0f2bea2832996039747b0cad
Trevor Augustine <122263629+TrevorA17@users.noreply.github.com>
2024-04-29 16:53:43 +0300

    api router

---
+++ RunPlumberAPI.R
@@ -0,0 +1,6 @@
+# This allows us to process a plumber API
+api <- plumber::plumb("PlumberAPI.R")
+
+#Run the API on a specific port ----
+# Specify a constant localhost port to use
+api$run(host = "127.0.0.1", port = 5022)
